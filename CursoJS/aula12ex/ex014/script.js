3d258a3d82e1e163b5b783bf5c504af6fd474440
jonasjps <jonaspsjw@hotmail.com>
2022-03-01 19:09:19 -0300

    Exercício modelo de JS

--- CursoJS/aula12ex/ex014/script.js
+++ CursoJS/aula12ex/ex014/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.` 
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'manha.png'
+        document.body.style.background ='#fdd868'
+    } else if ( hora >=12 && hora <18) {
+        //BOA TARDE
+        img.src = 'tarde.png'
+        document.body.style.background ='#e8947b'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.png'
+        document.body.style.background ='#121c25'
+    }
+}
