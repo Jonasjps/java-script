360ac41bdad4648dd25c684ab3110783e8ddba56
jonasjps <jonaspsjw@hotmail.com>
2022-03-05 11:35:02 -0300

    Treinado if, else JS

---
+++ CursoJS/aula11/ex009treinamento.js
@@ -0,0 +1,8 @@
+var pais ='japão'
+
+    if (pais == 'Brasil' || pais == 'brasil') {
+        console.log(`Você é Brasileiro`)
+    }else {
+        console.log(`Você é Estrangeiro`)
+    }
+    console.log(`Vivendo em ${pais}`)
