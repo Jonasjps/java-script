215dad0118f00cd6c2b7c447e36ab1a11021f3bc
jonasjps <jonaspsjw@hotmail.com>
2022-04-03 12:37:21 -0300

    aula 13 js repetições

---
+++ CursoJS/aula13/ambiente.js
@@ -0,0 +1,25 @@
+var c = 1
+do {
+    console.log(`tudo bem?`) 
+    c++
+}while (c <= 6)
+
+/*var c = 1
+while (c <= 6) {
+    console.log('tudo bem?') 
+    c++
+}
+*/
+
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
