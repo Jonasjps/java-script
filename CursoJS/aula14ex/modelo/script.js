10eada1611c962904832e7083adcabb5b1dcbaef
jonasjps <jonaspsjw@hotmail.com>
2022-04-04 13:10:01 -0300

    atualizando

--- CursoJS/aula14ex/modelo/script.js
+++ CursoJS/aula14ex/modelo/script.js
@@ -3,11 +3,9 @@ function Contar() {
     var fim = Number(document.querySelector('input#txtfim').value)
     var passo = Number(document.querySelector('input#txtpas').value)
     var res = Number(document.querySelector('div#result').value)
-    inicio=1
-    fim=10
-    passo=''
-    for( inicio = 1 ; inicio <= 10 ; inicio++) {
-        res.inne
+    var inicio = 1
+    while( inicio <= 10 ) {
+        res.innerHTML += `${passo}`
     }
     
 }
