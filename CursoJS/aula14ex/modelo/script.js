9682f5d1afd296940264e7e575da3875b1e3cefa
jonasjps <jonaspsjw@hotmail.com>
2022-04-04 08:00:11 -0300

    Aula14ex JS

---
+++ CursoJS/aula14ex/modelo/script.js
@@ -0,0 +1,13 @@
+function Contar() {
+    var inicio = Number(document.querySelector('input#txtini').value)
+    var fim = Number(document.querySelector('input#txtfim').value)
+    var passo = Number(document.querySelector('input#txtpas').value)
+    var res = Number(document.querySelector('div#result').value)
+    inicio=1
+    fim=10
+    passo=''
+    for( inicio = 1 ; inicio <= 10 ; inicio++) {
+        res.inne
+    }
+    
+}
