f26a945284c279ce27b83c1b8acc4f7a3ef49001
jonasjps <jonaspsjw@hotmail.com>
2022-04-04 23:27:09 -0300

    aula 14ex atualizado

--- CursoJS/aula14ex/modelo/script.js
+++ CursoJS/aula14ex/modelo/script.js
@@ -1,11 +1,30 @@
 function Contar() {
-    var inicio = Number(document.querySelector('input#txtini').value)
-    var fim = Number(document.querySelector('input#txtfim').value)
-    var passo = Number(document.querySelector('input#txtpas').value)
-    var res = Number(document.querySelector('div#result').value)
-    var inicio = 1
-    while( inicio <= 10 ) {
-        res.innerHTML += `${passo}`
-    }
+    let inicio = document.querySelector('input#txti')
+    let fim = document.querySelector('input#txtf')
+    let passo = document.querySelector('input#txtp')
+    let res = document.querySelector('div#result')
+    
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.passo == 0) {
+        res.innerHTML = 'Impossivel contar!'
+    }else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p <= 0) {
+            alert('Passo invalido! Considenrando passo 1')
+            p = 1
+        }
+        if(i < f) {
+            for(let c = i ; c <= f ; c += p) {
+               res.innerHTML += `${c} \u{1F449}` 
+            }
+        }else {
+            for(let c = i ; c >= f; c -= p)
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
+    } 
+    
     
 }
