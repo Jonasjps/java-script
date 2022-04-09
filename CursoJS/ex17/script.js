c5cb5e4e31cb3e34862a21753c49a98c7065a202
jonasjps <jonaspsjw@hotmail.com>
2022-04-08 22:39:11 -0300

    Ex17 js TABUADA

--- CursoJS/ex17/script.js
+++ CursoJS/ex17/script.js
@@ -1,1 +1,19 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    if(num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML= ''
+        while(c<=10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c}=  ${n*c}`
+            item. value = `tab ${item}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+}
