b06dfd7c2258376a83bcdc6a2e25cfd43416f82f
jonasjps <jonaspsjw@hotmail.com>
2022-04-12 23:30:47 -0300

    Praticando vetores

--- CursoJS/aula15/vertornatela.js
+++ CursoJS/aula15/vertornatela.js
@@ -1,5 +1,7 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
+valores.sort()
+
 //console.log(valores)
 /*
 console.log(valores[0])
@@ -8,8 +10,12 @@ console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
-*/
 
-for(let pos = 0 ; pos < valores.length ; pos++) {
+
+for(let pos = 0 ; pos < valores.length ; pos++) { //CODIGO SIMPLIFICADO
     console.log(`A posição ${pos} tem o valor ${valores [pos]}`)
+}
+*/
+for(let pos in valores) {
+    console.log(`A pasição ${pos} tem o valor ${valores[pos]}`)
 }
