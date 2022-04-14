5fddca9a4f5172b961687456c8b84283e6b9b0e8
jonasjps <jonaspsjw@hotmail.com>
2022-04-13 23:42:37 -0300

    Atualizando ex js

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -1,9 +1,15 @@
 let num = [ 5, 8, 2, 9, 3]
+
 num.push(1)
 num.sort()
 
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
-let pos = num.indexOf(8) 
-console.log(`O vetor 8 está na posição ${pos}`)
+let pos = num.indexOf(7) 
+if(pos == -1) {
+    console.log('O valor não foi encontrado')
+}else {
+    console.log(`O vetor 8 está na posição ${pos}`)
+}
+
  
