452df682c2d0d2abe41cb3a14ea6e0072f2b59d8
jonasjps <jonaspsjw@hotmail.com>
2022-04-14 23:38:52 -0300

    Atualizando ex  js

--- CursoJS/aula15/praticando.js
+++ CursoJS/aula15/praticando.js
@@ -14,7 +14,11 @@ console.log(num[2])
 console.log(num[3])
 console.log(num[4])
 console.log(num[5])
-*/
+
 for(let pos = 0 ; pos < num.length ; pos ++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}*/
+
+for(let pos in num) {
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
