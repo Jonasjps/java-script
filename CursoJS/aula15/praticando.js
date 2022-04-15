b2bf8edce59473b4f1e20e0b87227d26df0578b6
jonasjps <jonaspsjw@hotmail.com>
2022-04-14 23:35:03 -0300

    Praticando js

--- CursoJS/aula15/praticando.js
+++ CursoJS/aula15/praticando.js
@@ -1,5 +1,20 @@
 let num = [5, 8, 4, 9, 7]
+num.sort()
+/*
+
 num.push(10)
 
-console.log(`Aposição é ${num.indexOf(7)}`)
+console.log(num)
+//console.log(`Aposição é ${num.indexOf(7)}`)
 console.log(`O vetor tem ${num.length} posições!`)
+
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+console.log(num[5])
+*/
+for(let pos = 0 ; pos < num.length ; pos ++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
