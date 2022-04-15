0b0309db102b40db3f60527b9144001a9e416711
jonasjps <jonaspsjw@hotmail.com>
2022-04-14 23:40:29 -0300

    Atualizando

--- CursoJS/aula15/praticando.js
+++ CursoJS/aula15/praticando.js
@@ -19,6 +19,6 @@ for(let pos = 0 ; pos < num.length ; pos ++) {
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }*/
 
-for(let pos in num) {
+for(let pos in num) {//SIMPLIFICANDDO MEU CODIGO...
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
