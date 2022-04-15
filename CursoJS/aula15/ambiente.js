6d717c68165bfe35cd47e0d221440e1c8777004d
jonasjps <jonaspsjw@hotmail.com>
2022-04-14 23:55:47 -0300

    Atualizando pratica js

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -5,7 +5,7 @@ num.sort()
 
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
-let pos = num.indexOf(7) 
+let pos = num.indexOf(8) 
 if(pos == -1) {
     console.log('O valor não foi encontrado')
 }else {
--- CursoJS/aula15/praticando.js
+++ CursoJS/aula15/praticando.js
@@ -1,11 +1,11 @@
 let num = [5, 8, 4, 9, 7]
-num.sort()
-/*
+/*num.sort()
+
 
 num.push(10)
 
 console.log(num)
-//console.log(`Aposição é ${num.indexOf(7)}`)
+//
 console.log(`O vetor tem ${num.length} posições!`)
 
 console.log(num[0])
@@ -18,7 +18,14 @@ console.log(num[5])
 for(let pos = 0 ; pos < num.length ; pos ++) {
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }*/
-
-for(let pos in num) {//SIMPLIFICANDDO MEU CODIGO...
-    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+ let pos = num.indexOf(5)
+if(pos == -1){
+    console.log('Não encontrado!')
+}else {
+   console.log(`O vetor 9 esta na posição  ${num[pos]}`)
 }
+
+/*for(let pos in num) {//SIMPLIFICANDDO MEU CODIGO...
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+ }
+ */
