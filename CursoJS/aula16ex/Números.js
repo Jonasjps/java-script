473403d96f24ce98b7c0eec3bded770342b0bf44
jonasjps <jonaspsjw@hotmail.com>
2022-04-16 12:20:16 -0300

    Atualizando ex 16 js

--- CursoJS/aula16ex/Números.js
+++ CursoJS/aula16ex/Números.js
@@ -1,4 +1,4 @@
-let num = document.querySelector('input#num')
+let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#result')
 let valores = []
@@ -21,28 +21,45 @@ function inLista(n, l) {
 }
 
 function Adicionar( ) {
-    if(isNumero(num.value) && !inLista(num.value, valores)) {
-        alert('tudo OK')
+    if(isNumero(num.value) && !inLista(num.value, valores)) { // ! o sinal quer dizer não!
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text =`Valor ${num.value} adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor invalido ou já encontrado na lista.')
     }
+    num.value = ''// essa função esvazia o campo em que fica o numero no navegador
+    num.focus()//apos adicionar um numero o cursor continua piscando porque é onde meu foco esta!
 }
     
   
-    /*if(numero == 0 || numero > 100 ) {
-        alert('Invalido verifique os dados e tente novamente!')
-    }else{
-        let option = document.createElement('option')
-        option.text = `Valor ${numero} adicionado`
-        sel.value = `tab ${numero}`
-        sel.appendChild(option)
-        
-    }
-}
 function Finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores) {
+            soma += valores[pos]
+            if(valores[pos] > maior) 
+                maior = valores[pos]
+             if (valores[pos] < menor) 
+                menor = valores[pos]
+            
+        }
+        media = soma/tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números adicionados!</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</P>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML +=`<p>A média dos valores digitados é ${media}</p>`
+    }
     
-      res.innerHTML = 'Resultado final'
     
-    
-    
-}*/
+}
