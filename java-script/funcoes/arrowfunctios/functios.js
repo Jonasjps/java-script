a0c687e392f5cf92808adb89ff18dc432a80b976
jonasjps <jonaspsjw@hotmail.com>
2022-04-22 00:12:25 -0300

    Atualizando

---
+++ java-script/funcoes/arrowfunctios/functios.js
@@ -0,0 +1,3 @@
+const duplavelocidade = velocidade => velocidade * 2
+
+console.log(duplavelocidade(60))
---
+++ java-script/funcoes/funcoesanonimas/anonimas.js
@@ -0,0 +1,6 @@
+let duplavelocidade = function(velocidade) {
+    return velocidade * 2
+}
+
+let Novavelocidade = duplavelocidade(40)
+console.log(Novavelocidade)
