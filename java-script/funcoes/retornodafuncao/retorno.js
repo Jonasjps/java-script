76c0390e726c46e1a0f0ee4a8b25d83889b7cb0e
jonasjps <jonaspsjw@hotmail.com>
2022-04-23 19:46:24 -0300

    retorno da função

---
+++ java-script/funcoes/retornodafuncao/retorno.js
@@ -0,0 +1,16 @@
+function Acelerar(Velocidade,Aceleração) {
+    let Novavelocidade = Velocidade + Aceleração
+    console.log('Nova Velocidade: ' + Novavelocidade)
+    return Novavelocidade
+}
+console.log(Acelerar(90,10))
+/*
+let Velocidade = 80 
+let Aceleração = 5
+
+alert(Velocidade)
+
+Velocidade = Acelerar(Velocidade, Aceleração)
+
+alert(Velocidade)
+*/
