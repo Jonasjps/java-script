39ec9baa22dfa0578fa07d1d2051adf041a37983
jonasjps <jonaspsjw@hotmail.com>
2022-04-23 18:04:53 -0300

    Parâmetro da função

---
+++ java-script/funcoes/parametrosdaFuncao/parâmetro.js
@@ -0,0 +1,7 @@
+function Acelerar(Velocidade, Aceleração ) {
+    let Novavelocidade = Velocidade + Aceleração
+    alert(`Nova velocidade: ${Novavelocidade}` )
+}
+Acelerar(60,10)
+Acelerar(80,20)
+Acelerar(20,10)
