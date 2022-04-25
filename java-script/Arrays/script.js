cdb22703d530702d6be525aa870276cdbec19f7f
jonasjps <jonaspsjw@hotmail.com>
2022-04-25 07:35:41 -0300

    arrays js

---
+++ java-script/Arrays/script.js
@@ -0,0 +1,9 @@
+let NomedaNave = ['Jonas', 'Maria','José','Virginia']
+
+console.log(NomedaNave)
+
+let RemovendoNomes = NomedaNave.splice(1,2, 'Carol','Jesica','Jeferson')
+
+console.log(NomedaNave)
+
+console.log(RemovendoNomes)
