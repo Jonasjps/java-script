05042414dd0238c9dc84c23c9993a5555e688faa
jonasjps <jonaspsjw@hotmail.com>
2022-04-27 23:08:42 -0300

    simplificando código

--- java-script/Array-javascript/ex002/exercicio.js
+++ java-script/Array-javascript/ex002/exercicio.js
@@ -1,9 +1,16 @@
 let numeros = [ 1, 4, 5, 9, 14, 32 ]
 
-function duplonumero(num) {
+/*function duplonumero(num) {
     return num * 2
 }
 
 let duplonumeros = numeros.map(duplonumero)
 
 console.log(duplonumeros)
+
+*/
+
+
+let duplonumeros = numeros.map(num => num * 2)
+
+console.log(duplonumeros) // forma simplificada, usa menos codigos!
