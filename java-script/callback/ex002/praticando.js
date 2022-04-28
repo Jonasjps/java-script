c22b281def04ea96604630f3c34463be7bfe00ba
jonasjps <jonaspsjw@hotmail.com>
2022-04-27 21:28:48 -0300

    Praticando callbacks js

---
+++ java-script/callback/ex002/praticando.js
@@ -0,0 +1,12 @@
+function PrimeiroBloco() {
+    setTimeout(function() {
+        console.log('Este é o primeiro bloco de comandos após 5 segundos');
+    }, 10000);
+}
+
+function SegundoBloco() {
+    console.log('Este  é segundo bloco de comandos');
+}
+PrimeiroBloco()
+SegundoBloco()
+
