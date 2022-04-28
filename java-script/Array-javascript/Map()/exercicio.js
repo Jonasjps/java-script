73cead714be0cd5759819f3f16c2e549ccfd23b7
jonasjps <jonaspsjw@hotmail.com>
2022-04-28 08:01:05 -0300

    exercicio map() array js

---
+++ java-script/Array-javascript/Map()/exercicio.js
@@ -0,0 +1,11 @@
+let numeros = [1, 4, 9];
+//let raizes = numeros.map(Math.sqrt);
+
+console.log(numeros)
+//console.log(raizes)
+
+let duplos = numeros.map(num => num * 2
+)
+
+console.log(duplos)
+
