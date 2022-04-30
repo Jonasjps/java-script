c4fb6e871c7fad28279d99914d961fa8ecf2abf0
jonasjps <jonaspsjw@hotmail.com>
2022-04-29 23:07:59 -0300

    Atualizando ex

--- java-script/Array-javascript/Sort()/exercicio.js
+++ java-script/Array-javascript/Sort()/exercicio.js
@@ -4,10 +4,10 @@ let numeros = [6,3,4,100,2,5];
  
 console.log(numeros)
 console.log(numeros.sort(
-    (a,b) => {
+    (a,b) =>  a-b // forma simplificada
        /* if(a < b) return -1;
         if(a > b) return 1;
         return 0;*/
-        return a-b; // forma simplificada
-    } 
+        
+    
 ))
