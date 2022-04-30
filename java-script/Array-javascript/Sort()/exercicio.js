d3742bd4deca5edb9749854901e999f60902eb55
jonasjps <jonaspsjw@hotmail.com>
2022-04-29 22:03:59 -0300

    exercício método sort() array js

---
+++ java-script/Array-javascript/Sort()/exercicio.js
@@ -0,0 +1,12 @@
+let numeros = [6,3,4,100,2,5];
+
+// positivo, negativo ou 0
+ 
+console.log(numeros)
+console.log(numeros.sort(
+    (a,b) => {
+        if(a < b) return -1;
+        if(a > b) return 1;
+        return 0;
+    }
+))
