b14edde3f5ca4d5a11d83405fb38c3a1214abc44
jonasjps <jonaspsjw@hotmail.com>
2022-05-01 15:57:57 -0300

    Atualizando ex js

---
+++ java-script/Array-javascript/Find()/ex001.js
@@ -0,0 +1,5 @@
+let array = [2, 10, 15, 17, 20, 22, 30];
+let array1 = array.find(element => element > 17);
+
+console.log(array1);
+ console.log(array)//find não altera o array á qual foi acionado!
