4075f163a976c2e8859d0df9b30e6eb0c2d125ac
jonasjps <jonaspsjw@hotmail.com>
2022-05-01 16:25:52 -0300

    Exercício Find() array js

---
+++ java-script/Array-javascript/Find()/ex002.js
@@ -0,0 +1,12 @@
+
+// ENCONTRANDO UM OBJETO EM UM ARRAY POR UMA DE SUAS PROPRIEDADES.
+
+const iventario = [
+    {Nome: 'Banana' , Quantidade: 5},
+    {Nome: 'Maca' , Quantidade: 2},
+    {Nome: 'Pera' , Quantidade: 1}
+];
+
+const resultado = iventario.find(fruta => fruta.Nome === 'Maca');
+
+console.log(resultado);
