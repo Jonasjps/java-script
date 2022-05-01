ce0b89cf1bb8fc9ab5c2a3dfee3e08184d03026d
jonasjps <jonaspsjw@hotmail.com>
2022-05-01 17:04:37 -0300

    Exercício de array Join() js

---
+++ java-script/Array-javascript/Join()/ex001.js
@@ -0,0 +1,9 @@
+// JUNTANDO UM ARRAY  DE 3 FORMAS DIFERENTES...
+
+const elementos = ["Fire", "Air", "Water"];
+
+console.log(elementos.join())
+
+console.log(elementos.join(""))
+
+console.log(elementos.join("-"))
