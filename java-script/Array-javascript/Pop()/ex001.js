82c205b1767b66201a1e14ee00c558eca75fa38c
jonasjps <jonaspsjw@hotmail.com>
2022-05-01 23:39:04 -0300

    Exercício array pop() js

---
+++ java-script/Array-javascript/Pop()/ex001.js
@@ -0,0 +1,9 @@
+var Meupeixe = ['acara-bandeira', 'palhaça', 'mandarim', 'esturjão'];
+console.log(Meupeixe);
+
+var MeupeixePop = Meupeixe.pop();
+
+console.log(Meupeixe)
+console.log(MeupeixePop)
+
+
