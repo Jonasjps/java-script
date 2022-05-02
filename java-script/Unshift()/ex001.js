26ac1f520ff984ff8f615e0e32d67578e93a450c
jonasjps <jonaspsjw@hotmail.com>
2022-05-02 07:20:00 -0300

    Exercício de array Unshift() js

---
+++ java-script/Unshift()/ex001.js
@@ -0,0 +1,15 @@
+var array = [1, 2, 3, 4];
+
+console.log(array)
+
+array.unshift(0);
+
+console.log(array);
+
+array.unshift(-2,-1),
+
+console.log(array)
+
+array.unshift([-3]);
+
+console.log(array);
