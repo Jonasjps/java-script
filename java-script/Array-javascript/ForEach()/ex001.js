68322e7997a7c939aee6396990f7cf934fbab290
jonasjps <jonaspsjw@hotmail.com>
2022-05-02 22:25:54 -0300

    Atualizando ex

--- java-script/Array-javascript/ForEach()/ex001.js
+++ java-script/Array-javascript/ForEach()/ex001.js
@@ -15,4 +15,21 @@ listadedepessoasAprovadas.forEach((item,posicao,array) => { // lembrando que som
     console.log(`A quantidade de pessoas no proceso seletivo é: ${array.length}`)  
 });
 
-console.log(listadedepessoasAprovadas);     
+console.log(listadedepessoasAprovadas); 
+
+/*
+                        RESPOSTA
+Email para ada.lovelace@example.com foi enviado com sucesso!
+Sua posição é a de 0
+A quantidade de pessoas no proceso seletivo é: 4
+Email para maria.lovelace@example.com foi enviado com sucesso!
+Sua posição é a de 1
+A quantidade de pessoas no proceso seletivo é: 4
+Email para margaret.lovelace@example.com foi enviado com sucesso!
+Sua posição é a de 2
+A quantidade de pessoas no proceso seletivo é: 4
+Email para alan.lovelace@example.com foi enviado com sucesso!
+Sua posição é a de 3
+A quantidade de pessoas no proceso seletivo é: 4 
+
+*/
