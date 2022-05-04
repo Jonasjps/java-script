958a2b0cc80299e1f962e84bbc0d9dc89f551d73
jonasjps <jonaspsjw@hotmail.com>
2022-05-03 21:51:52 -0300

    Atualizando

--- java-script/Array-javascript/Some()/ex001.js
+++ java-script/Array-javascript/Some()/ex001.js
@@ -1,4 +1,6 @@
 
+
+
 let maiorque10 = [ 2, 5, 8, 1, 4, 12];
 
 let maior = maiorque10.some(element => element > 10);
@@ -7,3 +9,16 @@ let maior = maiorque10.some(element => element > 10);
 console.log(maior);
 
 // resposta = true
+
+/*
+let array = [5, 10, 15, 25, 2, 1, 4]
+
+let resultado = array.some(maiorque10)
+
+function maiorque10 (maior) {
+    return maior > 20;
+}
+
+
+console.log(resultado)
+*/
