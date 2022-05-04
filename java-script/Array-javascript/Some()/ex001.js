742abb974b91c197869fa4f88cf79d72c8b1aa97
jonasjps <jonaspsjw@hotmail.com>
2022-05-03 21:55:26 -0300

    Atualizando

--- java-script/Array-javascript/Some()/ex001.js
+++ java-script/Array-javascript/Some()/ex001.js
@@ -11,12 +11,14 @@ console.log(maior);
 // resposta = true
 
 /*
-let array = [5, 10, 15, 25, 2, 1, 4]
+Usando função Nomeada, tambem da o mesmo resultado.
+
+let array = [ 2, 5, 8, 1, 4, 12];
 
 let resultado = array.some(maiorque10)
 
 function maiorque10 (maior) {
-    return maior > 20;
+    return maior > 10;
 }
 
 
