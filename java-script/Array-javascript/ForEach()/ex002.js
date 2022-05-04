1f4169196c47a3c61b045fb7baa6ade31d1e2ce5
jonasjps <jonaspsjw@hotmail.com>
2022-05-04 13:09:43 -0300

    exercício array ForEach() Js

---
+++ java-script/Array-javascript/ForEach()/ex002.js
@@ -0,0 +1,5 @@
+const Redesocial = ['Youtube', 'Twitter', 'Instagram', 'Facebook']
+const arrow = (Redesocial,index, array) => {
+    console.log(Redesocial,index, array);
+}
+Redesocial.forEach(arrow)
