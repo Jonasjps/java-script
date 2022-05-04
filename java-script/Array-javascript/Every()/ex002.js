9ef1479c7ad0dc6c5a99b4040376e91e0e0730a8
jonasjps <jonaspsjw@hotmail.com>
2022-05-03 23:20:55 -0300

    Exercício de array Every() JS

---
+++ java-script/Array-javascript/Every()/ex002.js
@@ -0,0 +1,14 @@
+//Verifica se todos são maiores de idade.
+
+const lista = [
+    { id: 12, nome: 'Frederico', Idade: 18 },
+    { id: 47, nome: 'Francisca', Idade: 19 },
+    { id: 77, nome: 'Ramon', Idade: 48 },
+    { id: 85, nome: 'Jonas', Idade: 30 }
+];
+
+const resultado = lista.every(item => item.Idade >= 18);
+
+console.log(resultado);
+
+// resposta = true
