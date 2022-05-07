2af91a291a42fa0d4d0cea4436d5bc3dda1d70b1
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-07 10:25:26 -0300

    Exercício de array Concat() Js

---
+++ java-script/Array-javascript/Concat()/ex001.js
@@ -0,0 +1,7 @@
+let letras = ['a','b','c'];
+
+let numeros =  [1, 2, 3];
+
+let LetraseNumeros = numeros.concat(letras);
+
+console.log(LetraseNumeros);
---
+++ java-script/Array-javascript/Fill()/ex001.js
@@ -0,0 +1,4 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+array.fill(4);
+console.log(array);
