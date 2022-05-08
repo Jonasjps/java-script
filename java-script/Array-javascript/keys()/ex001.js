49ce3474c228202268974a88025156b9cafa954b
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-08 00:13:13 -0300

    Ex método  keys() js

---
+++ java-script/Array-javascript/keys()/ex001.js
@@ -0,0 +1,13 @@
+var arr = ['a', 'b', 'c'];
+var iterator = arr.keys();
+
+
+
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+
+
+
