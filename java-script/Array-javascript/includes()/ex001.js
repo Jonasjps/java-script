7dfee8a7063559e518bbc105e7cafb099aa59815
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-07 23:19:58 -0300

    Ex 001 método includes() Js

---
+++ java-script/Array-javascript/includes()/ex001.js
@@ -0,0 +1,26 @@
+var arr1 = [1, 2, 3];
+
+Novoarray = arr1.includes(2)
+console.log(Novoarray)
+
+Novoarray = arr1.includes(4)
+console.log(Novoarray)
+
+Novoarray = arr1.includes(3,3)
+console.log(Novoarray)
+
+Novoarray = arr1.includes(3, -1)
+console.log(Novoarray)
+
+/*
+Resposta
+
+true
+
+false
+
+false
+
+true
+
+*/ 
