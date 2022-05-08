08111a12048c224b2d9e943cb9f7bb30f0911afa
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-08 20:50:04 -0300

    exercício  Value() js

---
+++ java-script/Array-javascript/Values()/ex001.js
@@ -0,0 +1,19 @@
+//ITERAÇÃO USADO FOR ... OF
+
+var arr = ['w', 'y', 'k', 'o', 'p'];
+var eArr = arr.values();
+
+for (let letter of eArr) {
+    console.log(letter)
+};
+
+
+/*
+
+w
+y
+k
+o
+p
+
+*/
---
+++ java-script/Array-javascript/Values()/ex002.js
@@ -0,0 +1,10 @@
+//ITERAÇÃO ALTERNATIVA
+
+var arr = ['w', 'y', 'k', 'o', 'p'];
+var eArr = arr.values();
+
+console.log(eArr.next().value);
+console.log(eArr.next().value);
+console.log(eArr.next().value);
+console.log(eArr.next().value);
+console.log(eArr.next().value);
