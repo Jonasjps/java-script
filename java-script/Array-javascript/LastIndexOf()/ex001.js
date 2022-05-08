8d7c7fd7316e8ecd7a7b9615f8eb65dc174d13ad
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-08 19:21:31 -0300

    Exercício Método LastIndexOf() js

---
+++ java-script/Array-javascript/LastIndexOf()/ex001.js
@@ -0,0 +1,29 @@
+var array = [2, 5, 9, 2];
+console.log(array);
+
+Novoarray = array.lastIndexOf(2);
+console.log(Novoarray);
+
+Novoarray = array.lastIndexOf(7);
+console.log(Novoarray);
+
+Novoarray = array.lastIndexOf(2, 3);
+console.log(Novoarray);
+
+Novoarray = array.lastIndexOf(2, 2);
+console.log(Novoarray);
+
+Novoarray = array.lastIndexOf(2, -2);
+console.log(Novoarray);
+
+Novoarray = array.lastIndexOf(2, -1);
+console.log(Novoarray);
+
+/*
+3
+-1
+3
+0
+0
+3
+*/
