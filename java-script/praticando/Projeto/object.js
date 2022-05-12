9d9ecda56c0f72db418c61ebbcc8c446b8ecde91
Jonas Pessoa <93610493+Jonasjps@users.noreply.github.com>
2022-05-11 22:59:32 -0300

    execécio de javaScript objetos

---
+++ java-script/praticando/Projeto/object.js
@@ -0,0 +1,19 @@
+class Produto {
+
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um novo produto ')
+    }
+
+    deletar() {
+        alert('Produto deletato do estoque')
+    }
+}
+
+var produto = new Produto();
